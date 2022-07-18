2ff1eaecd1e96d239ce9f8172dacc9fa8c19c1b6
walterpisacco <65618158+walterpisacco@users.noreply.github.com>
2022-07-18 11:18:53 -0300

    Update config.py

--- config.py
+++ config.py
@@ -1,10 +1,9 @@
 class config():
 	def __init__(self):
-		self.servidor = 'desarrollo.aktis.com.ar'
-		#self.servidor = '127.0.0.1'
-		self.base = 'dsv_dev'
-		self.usuario = 'admin_iothinks'
-		self.password = 'Global*3522'
+		self.servidor = ''
+		self.base = ''
+		self.usuario = ''
+		self.password = ''
 		self.pathModelos = 'ai/conv/'
 		self.urlApi = 'http://localhost:4001/'
 		
